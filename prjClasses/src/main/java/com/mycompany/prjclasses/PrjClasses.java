cb81aface788f546e448a41cd1ef93301db5a3de
ST10472588 <ST10472588@vcconnect.edu.za>
2025-03-11 10:39:03 +0200

    added new class
    added new classes

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,8 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! I just did my 1st github along with Reese");
+        newClass nc = new newClass();//creaing an object of newClass
+        nc.helloClass();//have to use variable name nc to communicate with class
+        System.out.println(nc.strSecondClassVar);//output the variable from the second class. 
     }
 }
